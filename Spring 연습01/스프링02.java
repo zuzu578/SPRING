fe82709951b485de4063b6a670654d212e950318
Napalm <69393030+dlwnghks6821@users.noreply.github.com>
2020-12-05 15:35:56 +0900

    스프링 DI, 스프링 개념 예제

---
+++ Spring 연습01/스프링02.java
@@ -0,0 +1,38 @@
+package com.board.controller;
+
+public class MyCalculator {
+	private Calculator calculator;
+	private int firstNum;
+	private int secondNum;
+	public Calculator getCalculator() {
+		return calculator;
+	}
+	public void setCalculator(Calculator calculator) {
+		this.calculator = calculator;
+	}
+	public int getFirstNum() {
+		return firstNum;
+	}
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+	public int getSecondNum() {
+		return secondNum;
+	}
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+	
+	public void add() {
+		calculator.additon(firstNum,secondNum);
+	}
+	public void sub() {
+		calculator.subtraction(firstNum,secondNum);
+	}
+	public void multi() {
+		calculator.multiplication(firstNum,secondNum);
+	}
+	public void div() {
+		calculator.division(firstNum,secondNum);
+	}
+}
