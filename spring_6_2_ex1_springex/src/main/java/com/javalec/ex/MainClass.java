70d1ecf1cc3acc261f5ca9223412338ceb3447e4
Napalm <69393030+dlwnghks6821@users.noreply.github.com>
2020-12-07 17:38:43 +0900

    자바에서 DI 주입 annotation 사용

---
+++ spring_6_2_ex1_springex/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,27 @@
+package com.javalec.ex;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		
+		Student student1 = ctx.getBean("student1", Student.class);
+		System.out.println("이름 : " + student1.getName());
+		System.out.println("나이 : " + student1.getAge());
+		System.out.println("취미 : " + student1.getHobbys());
+		System.out.println("신장 : " + student1.getHeight());
+		System.out.println("몸무게 : " + student1.getWeight());
+		
+		Student student2 = ctx.getBean("student2", Student.class);
+		System.out.println("이름 : " + student2.getName());
+		System.out.println("나이 : " + student2.getAge());
+		System.out.println("취미 : " + student2.getHobbys());
+		System.out.println("신장 : " + student2.getHeight());
+		System.out.println("몸무게 : " + student2.getWeight());
+		
+		ctx.close();
+	}
+	
+}
